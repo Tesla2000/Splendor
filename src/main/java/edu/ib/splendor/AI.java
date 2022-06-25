29868eefe1a8c1e2831eb61cc38863e25e374681
Tesla2000 <fratajczak124@gmail.com>
2022-06-25 21:57:10 +0200

    Added conversion to machine interpretable form

--- src/main/java/edu/ib/splendor/AI.java
+++ src/main/java/edu/ib/splendor/AI.java
@@ -8,8 +8,8 @@ import java.util.*;
 public class AI {
     private static boolean lost;
     private static boolean won;
-    private static int gameCounter = 0;
-//    public static boolean aristocrat;
+    private static int gameCounter = 2000;
+    //    public static boolean aristocrat;
     private static List<int[]> moves;
     private final BoardController boardController;
     private final HashMap<Integer, Move> possibleMoves;
@@ -54,37 +54,37 @@ public class AI {
         possibleMoves.put(26, new BuildBuilding(boardController, Tier.THIRD, 0));
     }
 
-    public boolean playMove(int move){
+    public boolean playMove(int move) {
         if (!possibleMoves.containsKey(move)) throw new IllegalArgumentException("There is no such move");
         Move toPlay = possibleMoves.get(move);
-        if (move<10 && validateResourcesOne(((GetGem) toPlay).getGemList())&& player.getPossession().values().stream().reduce(Integer::sum).stream().toList().get(0) < 8){
+        if (move < 10 && validateResourcesOne(((GetGem) toPlay).getGemList()) && player.getPossession().values().stream().reduce(Integer::sum).stream().toList().get(0) < 8) {
             toPlay.play(board, player);
             return true;
-        }
-        else if (move<15 && move > 9 && validateResourcesTwo(((GetGem) toPlay).getGemList()) && player.getPossession().values().stream().reduce(Integer::sum).stream().toList().get(0) < 9){
+        } else if (move < 15 && move > 9 && validateResourcesTwo(((GetGem) toPlay).getGemList()) && player.getPossession().values().stream().reduce(Integer::sum).stream().toList().get(0) < 9) {
             toPlay.play(board, player);
             return true;
-        }
-        else if (move>14 && boardController.canBuy(((BuildBuilding) toPlay).getTier(), ((BuildBuilding) toPlay).getIndex(), board, player)) {
+        } else if (move > 14 && boardController.canBuy(((BuildBuilding) toPlay).getTier(), ((BuildBuilding) toPlay).getIndex(), board, player)) {
             toPlay.play(board, player);
 //            boardController.endTurn(board, player);
-            if (player.getPoints()>= 15) won = true;
+            if (player.getPoints() >= 15) won = true;
             return true;
         }
         return false;
     }
 
-    public void playTurn(){
+    public void playTurn(boolean manual) {
         Random random = new Random();
         HashMap<Double, Integer> order = new HashMap<>();
         double[] keys = new double[27];
-        for (int i=0; i<27;i++){
-            double value = random.nextDouble();
-            order.put(value, i);
-            keys[i] = value;
+        if (!manual) {
+            for (int i = 0; i < 27; i++) {
+                double value = random.nextDouble();
+                order.put(value, i);
+                keys[i] = value;
+            }
+            Arrays.sort(keys);
         }
-        Arrays.sort(keys);
-        int[] state = new int[170];
+        int[] state = new int[172];
         state[1] = player.getPoints();
         state[2] = player.getReserveNumber();
         state[3] = player.getPossession().get(Gem.BLUE);
@@ -105,116 +105,191 @@ public class AI {
         state[18] = board.getStored(Gem.WHITE);
         state[19] = board.getStored(Gem.GOLD);
         for (int i = 0; i < 4; i++) {
-            state[20 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getPoints();
-            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.BLUE) state[21 + 11*i] = 1;else state[21 + 11*i] = 0;
-            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.BROWN) state[22 + 11*i] = 1;else state[22 + 11*i] = 0;
-            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.GREEN) state[23 + 11*i] = 1;else state[23 + 11*i] = 0;
-            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.RED) state[24 + 11*i] = 1;else state[24 + 11*i] = 0;
-            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.WHITE) state[25 + 11*i] = 1;else state[25 + 11*i] = 0;
-            state[26 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.BLUE);
-            state[27 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.BROWN);
-            state[28 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.GREEN);
-            state[29 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.RED);
-            state[30 + 11*i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.WHITE);
+            state[20 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getPoints();
+            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.BLUE) state[21 + 11 * i] = 1;
+            else state[21 + 11 * i] = 0;
+            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.BROWN) state[22 + 11 * i] = 1;
+            else state[22 + 11 * i] = 0;
+            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.GREEN) state[23 + 11 * i] = 1;
+            else state[23 + 11 * i] = 0;
+            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.RED) state[24 + 11 * i] = 1;
+            else state[24 + 11 * i] = 0;
+            if (board.getTradeRow().getTierFirstVisible()[i].getProduction() == Gem.WHITE) state[25 + 11 * i] = 1;
+            else state[25 + 11 * i] = 0;
+            state[26 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.BLUE);
+            state[27 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.BROWN);
+            state[28 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.GREEN);
+            state[29 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.RED);
+            state[30 + 11 * i] = board.getTradeRow().getTierFirstVisible()[i].getCost().get(Gem.WHITE);
         }
         for (int i = 0; i < 4; i++) {
-            state[64 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getPoints();
-            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.BLUE) state[65 + 11*i] = 1;else state[65 + 11*i] = 0;
-            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.BROWN) state[66 + 11*i] = 1;else state[66 + 11*i] = 0;
-            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.GREEN) state[67 + 11*i] = 1;else state[67 + 11*i] = 0;
-            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.RED) state[68 + 11*i] = 1;else state[68 + 11*i] = 0;
-            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.WHITE) state[69 + 11*i] = 1;else state[69 + 11*i] = 0;
-            state[70 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.BLUE);
-            state[71 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.BROWN);
-            state[72 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.GREEN);
-            state[73 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.RED);
-            state[74 + 11*i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.WHITE);
+            state[64 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getPoints();
+            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.BLUE) state[65 + 11 * i] = 1;
+            else state[65 + 11 * i] = 0;
+            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.BROWN) state[66 + 11 * i] = 1;
+            else state[66 + 11 * i] = 0;
+            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.GREEN) state[67 + 11 * i] = 1;
+            else state[67 + 11 * i] = 0;
+            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.RED) state[68 + 11 * i] = 1;
+            else state[68 + 11 * i] = 0;
+            if (board.getTradeRow().getTierSecondVisible()[i].getProduction() == Gem.WHITE) state[69 + 11 * i] = 1;
+            else state[69 + 11 * i] = 0;
+            state[70 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.BLUE);
+            state[71 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.BROWN);
+            state[72 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.GREEN);
+            state[73 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.RED);
+            state[74 + 11 * i] = board.getTradeRow().getTierSecondVisible()[i].getCost().get(Gem.WHITE);
         }
         for (int i = 0; i < 4; i++) {
-            state[108 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getPoints();
-            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.BLUE) state[109 + 11*i] = 1;else state[109 + 11*i] = 0;
-            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.BROWN) state[110 + 11*i] = 1;else state[110 + 11*i] = 0;
-            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.GREEN) state[111 + 11*i] = 1;else state[111 + 11*i] = 0;
-            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.RED) state[112 + 11*i] = 1;else state[112 + 11*i] = 0;
-            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.WHITE) state[113 + 11*i] = 1;else state[113 + 11*i] = 0;
-            state[114 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.BLUE);
-            state[115 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.BROWN);
-            state[116 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.GREEN);
-            state[117 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.RED);
-            state[118 + 11*i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.WHITE);
+            state[108 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getPoints();
+            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.BLUE) state[109 + 11 * i] = 1;
+            else state[109 + 11 * i] = 0;
+            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.BROWN) state[110 + 11 * i] = 1;
+            else state[110 + 11 * i] = 0;
+            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.GREEN) state[111 + 11 * i] = 1;
+            else state[111 + 11 * i] = 0;
+            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.RED) state[112 + 11 * i] = 1;
+            else state[112 + 11 * i] = 0;
+            if (board.getTradeRow().getTierThirdVisible()[i].getProduction() == Gem.WHITE) state[113 + 11 * i] = 1;
+            else state[113 + 11 * i] = 0;
+            state[114 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.BLUE);
+            state[115 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.BROWN);
+            state[116 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.GREEN);
+            state[117 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.RED);
+            state[118 + 11 * i] = board.getTradeRow().getTierThirdVisible()[i].getCost().get(Gem.WHITE);
         }
-        for (int i = 0; i < board.getAristocrats().size(); i++){
-            state[150+5*i] = board.getAristocrats().get(i).getBlue();
-            state[151+5*i] = board.getAristocrats().get(i).getBrown();
-            state[152+5*i] = board.getAristocrats().get(i).getGreen();
-            state[153+5*i] = board.getAristocrats().get(i).getRed();
-            state[154+5*i] = board.getAristocrats().get(i).getWhite();
+        for (int i = 0; i < board.getAristocrats().size(); i++) {
+            state[152 + 5 * i] = board.getAristocrats().get(i).getBlue();
+            state[153 + 5 * i] = board.getAristocrats().get(i).getBrown();
+            state[154 + 5 * i] = board.getAristocrats().get(i).getGreen();
+            state[155 + 5 * i] = board.getAristocrats().get(i).getRed();
+            state[156 + 5 * i] = board.getAristocrats().get(i).getWhite();
         }
-        for (int i = 26; i > -1; i--){
-            if (playMove(order.get(keys[i]))) {
-                state[0] = order.get(keys[i]);
-                moves.add(state);
-//                if (aristocrat) moves.add(new int[]{});
-                return;
-            }
+        for (int i = 26; i > -1; i--) {
+            if (!manual) {
+                if (playMove(order.get(keys[i]))) {
+                    state[0] = order.get(keys[i]);
+                    moves.add(state);
+                    return;
+                }
+            } else {
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Play the next move: ");
+                    int move = scanner.nextInt();
+                    playMove(move);
+                    scanner.close();
+                    state[0] = move;
+                    moves.add(state);
+                    System.out.print("[");
+                    for (int s : state) {
+                        System.out.print(s + ", ");
+                    }
+                    System.out.print("]");
+                    return;
+                }
         }
 //        throw new IllegalArgumentException("No possible moves");
         lost = true;
     }
 
     private boolean validateResourcesTwo(Gem[] gemList) {
-        return board.getStored(gemList[0])>3;
+        return board.getStored(gemList[0]) > 3;
     }
 
     private boolean validateResourcesOne(Gem[] gemList) {
-        for (Gem gem: gemList){
-            if (board.getStored(gem) < 1){
-               return false;
+        for (Gem gem : gemList) {
+            if (board.getStored(gem) < 1) {
+                return false;
             }
         }
         return true;
     }
 
     public static void main(String[] args) throws IOException {
-        while (gameCounter<10000){
-        ArrayList<ArrayList<Card>> cards = GenerateDeck.generateCards();
-        TradeRow tradeRow = new TradeRow(cards.get(0),cards.get(1),cards.get(2));
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player("Skynet"));
-        AI ai = new AI(new BoardController(), new Board(tradeRow, players,  7,7,7,7,7,5));
-        while (true) {
-            ai.playTurn();
-            if (lost) {
-                break;
+        while (gameCounter < 10000) {
+            ArrayList<ArrayList<Card>> cards = GenerateDeck.generateCards();
+            TradeRow tradeRow = new TradeRow(cards.get(0), cards.get(1), cards.get(2));
+            ArrayList<Player> players = new ArrayList<>();
+            players.add(new Player("Skynet"));
+            AI ai = new AI(new BoardController(), new Board(tradeRow, players, 7, 7, 7, 7, 7, 5));
+            while (true) {
+                ai.playTurn(false);
+                if (lost) {
+                    break;
+                }
+                if (won) {
+                    saveToFile();
+                    break;
+                }
             }
-            if (won) {
-                saveToFile();
-                break;
-            }
-        }
         }
     }
 
+    private Integer[] convertStateToMachineInput(Integer[] state) {
+        List<Integer> MachineInput = Arrays.asList(state);
+        for (int j = 0; j < 12; j++) {
+            for (int i = 0; i < 5; i++) {
+                int cost = state[26 + i + 11 * j];
+                int production = state[9 + i];
+                MachineInput.set(26 + i + 11 * j, Math.max(0, cost - production));
+                MachineInput.add(cost - production - state[3 + i]);
+            }
+        }
+        for (int i = 0; i < 12; i++) {
+            for (int k = 0; k < 5; k++) {
+                if (MachineInput.get(21 + k + 11 * i) != 0){
+                    MachineInput.set(21 + k + 11 * i, 0);
+                    for (int j=0; j<12; j++){
+                        if (MachineInput.get(26 + 11 * j + k) != 0)
+                            MachineInput.set(21 + k + 11 * i, MachineInput.get(26 + 11 * j + k) + 1);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (MachineInput.get(21 + j + 11 * i) != 0){
+                    int sum = 0;
+                    for (int k=0; k<4; k++){
+                        if (MachineInput.get(150 + j + 5*k) - MachineInput.get(9 + j)>0)
+                            sum++;
+                    }
+                    MachineInput.add(sum);
+                    break;
+                }
+                MachineInput.add(0);
+            }
+        }
+        int move = MachineInput.get(0);
+        MachineInput.remove(0);
+        for (int i=0;i<27;i++)
+        MachineInput.add(0, 0);
+        MachineInput.set(move, 1);
+        return MachineInput.toArray(new Integer[0]);
+    }
+
+
     private static void saveToFile() throws IOException {
         if (moves.size() <= 25) {
-        File file = new File("C:\\Users\\Dell\\IdeaProjects\\Splendor\\src\\main\\java\\edu\\ib\\splendor\\results\\"+moves.size()+"\\"+gameCounter+".txt");
-        FileWriter writer = null;
-        gameCounter++;
-        try {
-            writer = new FileWriter(file);
-            for (int[] state : moves) {
-                for (int element : state) {
-                    writer.write(String.valueOf(element));
-                    writer.write(",");
+            File file = new File("C:\\Users\\Dell\\IdeaProjects\\Splendor\\src\\main\\java\\edu\\ib\\splendor\\results\\" + moves.size() + "\\" + gameCounter + ".txt");
+            FileWriter writer = null;
+            gameCounter++;
+            try {
+                writer = new FileWriter(file);
+                for (int[] state : moves) {
+                    for (int element : state) {
+                        writer.write(String.valueOf(element));
+                        writer.write(",");
+                    }
+                    writer.write("\n");
                 }
-                writer.write("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (writer != null) {
+                    writer.close();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }}
+        }
     }
 }
