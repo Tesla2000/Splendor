2bd7fcfb175f546a1350517fbfcd7ad39d5001da
Tesla2000 <90644163+Tesla2000@users.noreply.github.com>
2022-07-15 08:08:12 +0200

    Update Aristocrat.java

--- src/main/java/edu/ib/splendor/Aristocrat.java
+++ src/main/java/edu/ib/splendor/Aristocrat.java
@@ -3,38 +3,18 @@ package edu.ib.splendor;
 import java.util.HashMap;
 
 public record Aristocrat(int red, int green, int blue, int brown, int white, String image) {
-
-    public int getRed() {
-        return red;
-    }
-
-    public int getGreen() {
-        return green;
-    }
-
-    public int getBlue() {
-        return blue;
-    }
-
-    public int getBrown() {
-        return brown;
-    }
-
-    public int getWhite() {
-        return white;
-    }
+    HashMap<Gem, Integer> cost = new HashMap<>();
+    cost.put(Gem.BLUE, blue);
+    cost.put(Gem.RED, red);
+    cost.put(Gem.GREEN, green);
+    cost.put(Gem.BROWN, brown);
+    cost.put(Gem.WHITE, white);
 
     public int getPoints() {
         return 3;
     }
 
     public HashMap<Gem, Integer> getCost(){
-        HashMap<Gem, Integer> cost = new HashMap<>();
-        cost.put(Gem.BLUE, blue);
-        cost.put(Gem.RED, red);
-        cost.put(Gem.GREEN, green);
-        cost.put(Gem.BROWN, brown);
-        cost.put(Gem.WHITE, white);
         return cost;
     }
 
