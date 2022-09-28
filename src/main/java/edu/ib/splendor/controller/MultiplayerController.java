cea28d85e6571503b7c07774b322feeead21601e
Tesla2000 <fratajczak124@gmail.com>
2022-09-28 22:23:20 +0200

    fixed joining game and allowed game hosting without credentials

--- src/main/java/edu/ib/splendor/controller/MultiplayerController.java
+++ src/main/java/edu/ib/splendor/controller/MultiplayerController.java
@@ -33,12 +33,11 @@ public class MultiplayerController {
         String css = Objects.requireNonNull(getClass().getClassLoader().getResource("board.css")).toExternalForm();
         scene.getStylesheets().add(css);
         stage.show();
-
     }
 
     @FXML
     void onlineGame(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("hostingRoom.fxml")));
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setTitle("Splendor");
@@ -46,7 +45,6 @@ public class MultiplayerController {
         String css = Objects.requireNonNull(getClass().getClassLoader().getResource("board.css")).toExternalForm();
         scene.getStylesheets().add(css);
         stage.show();
-
     }
 
     @FXML
