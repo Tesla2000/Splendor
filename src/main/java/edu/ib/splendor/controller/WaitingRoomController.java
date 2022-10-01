c199e7e69dd2e141e7e2fadee1ad3e7f8905263f
Tesla2000 <fratajczak124@gmail.com>
2022-10-01 16:17:10 +0200

    Removed option of online since it is not functioning as well as I intend eventually switching to sockets will be a better call

--- src/main/java/edu/ib/splendor/controller/WaitingRoomController.java
+++ src/main/java/edu/ib/splendor/controller/WaitingRoomController.java
@@ -22,18 +22,17 @@ public class WaitingRoomController {
     @FXML
     private URL location;
 
-    @FXML
-    void admin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getClassLoader().getResource("multiplayer.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setTitle("Splendor");
-        stage.setScene(scene);
-        String css = getClass().getClassLoader().getResource("board.css").toExternalForm();
-        scene.getStylesheets().add(css);
-        stage.show();
-
-    }
+//    @FXML
+//    void admin(ActionEvent event) throws IOException {
+//        root = FXMLLoader.load(getClass().getClassLoader().getResource("multiplayer.fxml"));
+//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setTitle("Splendor");
+//        stage.setScene(scene);
+//        String css = getClass().getClassLoader().getResource("board.css").toExternalForm();
+//        scene.getStylesheets().add(css);
+//        stage.show();
+//    }
 
     @FXML
     void user(ActionEvent event) throws IOException {
